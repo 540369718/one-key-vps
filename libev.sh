ea809785b6f11a4649b15c6f0775e7db8c4db39a
Nero <zhuyanlong@nuaa.edu.cn>
2018-06-18 23:14:46 +0800

    Update libev.sh

--- libev.sh
+++ libev.sh
@@ -17,46 +17,46 @@ echo ""
 
 # Install SS
 function install_SS(){
-	check_sys
+    check_sys
     rootness
     get_my_ip
     install_env
-	install_libev
+    install_libev
     install_lotServer
-	install_status_client
+    install_status_client
     show_doc
 }
 
 # Make sure only root can run our script
 function rootness(){
 if [[ $EUID -ne 0 ]]; then
-   echo "Error:This script must be run as root!" 1>&2
-   exit 1
+    echo "Error:This script must be run as root!" 1>&2
+    exit 1
 fi
 }
 
 # Check release
 check_sys(){
-	if [[ -f /etc/redhat-release ]]; then
-		release="centos"
-	elif cat /etc/issue | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
-	elif cat /proc/version | grep -q -E -i "debian"; then
-		release="debian"
-	elif cat /proc/version | grep -q -E -i "ubuntu"; then
-		release="ubuntu"
-	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-		release="centos"
+    if [[ -f /etc/redhat-release ]]; then
+        release="centos"
+    elif cat /etc/issue | grep -q -E -i "debian"; then
+        release="debian"
+    elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+        release="ubuntu"
+    elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
+    elif cat /proc/version | grep -q -E -i "debian"; then
+        release="debian"
+    elif cat /proc/version | grep -q -E -i "ubuntu"; then
+        release="ubuntu"
+    elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
     fi
-	bit=`uname -m`
-	if [[ ${release} = "debian" ]]; then
-		version=`cat /etc/debian_version`
-		apt install -y sudo		
-	fi
+    bit=`uname -m`
+    if [[ ${release} = "debian" ]]; then
+        version=`cat /etc/debian_version`
+        apt install -y sudo		
+    fi
 }
 
 # Get IP address of the server
@@ -67,44 +67,43 @@ function get_my_ip(){
 
 # Install necessary lib
 function install_env(){
-	apt-get install curl vim wget python-dev libffi-dev python-setuptools -y
-	curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py && rm get-pip.py
-	pip install paramiko psutil
+    apt-get install curl vim wget python-dev libffi-dev python-setuptools -y
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py && rm get-pip.py
+    pip install paramiko psutil
 }
 
 function install_libev(){
-	if [[ ${release} = "debian" ]]; then
-		if [[ $version > "8" && $version < "9" ]];then
-			install_libev_debian_Jessie
-		elif [[ $version > "9" && $version < "10" ]];then
-			install_libev_debian_Stretch
-		fi		
-	elif [[ ${release} = "ubuntu" ]]; then
-		install_libev_ubuntu
-	fi
-	
-	wget -O /etc/init.d/shadowsocks-manager https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-manager  
-	chmod +x /etc/init.d/shadowsocks-manager && update-rc.d -f shadowsocks-manager defaults  
-	mkdir /etc/shadowsocks-manager
-	sample_config
-	/etc/init.d/shadowsocks-manager start		  
+    if [[ ${release} = "debian" ]]; then
+        if [[ $version > "8" && $version < "9" ]];then
+            install_libev_debian_Jessie
+        elif [[ $version > "9" && $version < "10" ]];then
+            install_libev_debian_Stretch
+        fi		
+    elif [[ ${release} = "ubuntu" ]]; then
+        install_libev_ubuntu
+    fi
+    wget -O /etc/init.d/shadowsocks-manager https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-manager  
+    chmod +x /etc/init.d/shadowsocks-manager && update-rc.d -f shadowsocks-manager defaults  
+    mkdir /etc/shadowsocks-manager
+    sample_config
+    /etc/init.d/shadowsocks-manager start		  
 }
 function install_libev_debian_Jessie(){
-	sudo sh -c 'printf "deb http://deb.debian.org/debian jessie-backports main\n" > /etc/apt/sources.list.d/jessie-backports.list'
-	sudo sh -c 'printf "deb http://deb.debian.org/debian jessie-backports-sloppy main" >> /etc/apt/sources.list.d/jessie-backports.list'
-	sudo apt update
-	sudo apt -t jessie-backports-sloppy install shadowsocks-libev
+    sudo sh -c 'printf "deb http://deb.debian.org/debian jessie-backports main\n" > /etc/apt/sources.list.d/jessie-backports.list'
+    sudo sh -c 'printf "deb http://deb.debian.org/debian jessie-backports-sloppy main" >> /etc/apt/sources.list.d/jessie-backports.list'
+    sudo apt update
+    sudo apt -t jessie-backports-sloppy install shadowsocks-libev
 }
 function install_libev_debian_Stretch(){
-	sudo sh -c 'printf "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list'
-	sudo apt update
-	sudo apt -t stretch-backports install shadowsocks-libev
+    sudo sh -c 'printf "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list'
+    sudo apt update
+    sudo apt -t stretch-backports install shadowsocks-libev
 }
 function install_libev_ubuntu(){
-	sudo apt-get install software-properties-common -y
-	sudo add-apt-repository ppa:max-c-lv/shadowsocks-libev -y
-	sudo apt-get update
-	sudo apt install shadowsocks-libev
+    sudo apt-get install software-properties-common -y
+    sudo add-apt-repository ppa:max-c-lv/shadowsocks-libev -y
+    sudo apt-get update
+    sudo apt install shadowsocks-libev
 }
 function sample_config(){
     echo "{
@@ -124,11 +123,11 @@ function sample_config(){
 }
 
 function install_lotServer(){
-	wget -N --no-check-certificate -O appex.sh "https://raw.githubusercontent.com/0oVicero0/serverSpeeder_Install/master/appex.sh" && bash appex.sh 'install'		
+    wget -N --no-check-certificate -O appex.sh "https://raw.githubusercontent.com/0oVicero0/serverSpeeder_Install/master/appex.sh" && bash appex.sh 'install'		
 }
 
 function install_status_client(){
-	wget -N --no-check-certificate https://raw.githubusercontent.com/540369718/ServerStatus/master/shell/status_client.sh && chmod +x status_client.sh && bash status_client.sh
+    wget -N --no-check-certificate https://raw.githubusercontent.com/540369718/ServerStatus/master/shell/status_client.sh && chmod +x status_client.sh && bash status_client.sh
 }
 
 function show_doc(){
